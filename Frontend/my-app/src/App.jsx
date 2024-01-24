633123112c498b3b265f45b19d26b434ba13ca6e
Mohamed-Hajri <hajr.kef@gmail.com>
2024-01-24 15:11:42 +0100

    '7'

--- Frontend/my-app/src/App.jsx
+++ Frontend/my-app/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
-//import Sports from './components/Sports';
+import Sports from './components/Sports';
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
       <Home/>
+      <Sports/>
     </div>
   );
 }
