6f5f92c521e8d2121239c0b775f741f0d0d7c7a5
Mohamed-Hajri <hajr.kef@gmail.com>
2024-01-25 09:47:16 +0100

    /|\

--- Frontend/my-app/src/components/Navbar.jsx
+++ Frontend/my-app/src/components/Navbar.jsx
@@ -1,37 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
-    import Nav from 'react-bootstrap/Nav';
-    import Navbar from 'react-bootstrap/Navbar';
-    import NavDropdown from 'react-bootstrap/NavDropdown';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Sports from './Sports';
+import Home from './Home';
+import TV from './Tv';
+import Radio from './Radio';
+import About from './About';
+import WeatherInfo from './WeatherInfo';
 
-    
-    
-    function BasicExample() {
-      return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-          <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      );
-    }
-    
+
+
+function BasicExample() {
+  const [activeTab, setActiveTab] = useState('home');
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <div>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">Tunisian Press</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home" onClick={() => handleTabClick('home')}>
+                Home
+              </Nav.Link>
+              <Nav.Link href="#sports" onClick={() => handleTabClick('sports')}>
+                Sports
+              </Nav.Link>
+              <Nav.Link href="#link" onClick={() => handleTabClick('politics')}>
+                Politics
+              </Nav.Link>
+              <Nav.Link href="#link" onClick={() => handleTabClick('tech')}>
+                Tech
+              </Nav.Link>
+
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#weather" onClick={() => handleTabClick('Weather')}>
+                  Weather
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#radio" onClick={() => handleTabClick('radio')}>
+                  Radio
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#tv" onClick={() => handleTabClick('tv')}>
+                  TV
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#about" onClick={() => handleTabClick('about')}>
+                  About Us
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {activeTab === 'home' && <Home />}
+      {activeTab === 'sports' && <Sports />}
+      {activeTab === 'tv' && <TV />}
+      {activeTab === 'radio' && <Radio />}
+      {activeTab === 'about' && <About />}
+      {activeTab === 'Weather' && <WeatherInfo/>}
+
+    </div>
+  );
+}
+
 export default BasicExample;
---
+++ Frontend/my-app/src/components/Radio.jsx
@@ -0,0 +1,19 @@
+// Radio.jsx
+import React from 'react';
+import './Radio.css';
+
+const Radio = () => {
+  return (
+    <div className="radio-container">
+      <audio controls>
+        {/* Replace with your actual radio stream URL */}
+        <img src='./images/free-palestine.jpg'></img>
+        <source src="https://scdn.nrjaudio.fm/adwz2/fr/30001/mp3_128.mp3?origine=fluxradios" type="audio/mp3" />
+
+        Your browser does not support the audio tag.
+      </audio>
+    </div>
+  );
+}
+
+export default Radio;
